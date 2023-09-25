70d235cd9fed615ee02f85817b21136523a1a7e7
anadolacio <anadolacio@hotmail.com>
2023-09-25 18:58:23 -0300

    resolução requisito 4

--- news/views.py
+++ news/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import News
 
 # Create your views here.
+
+
+def home_page(request):
+    all_news = News.objects.all()
+    new = {"news_list": all_news}
+    return render(request, "home.html", new)
